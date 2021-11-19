b7e1d5e4ce628e1dfc60c8e0558b410a9383a2f6
serratoangel <serratoariasangel@gmail.com>
2021-11-18 21:08:38 -0500

    es6 sololearn tutorial

--- javascript.js
+++ javascript.js
@@ -1,3 +1,24 @@
-function mifuncion(){ /* This part is linked with an exercise in the HTML file */ 
+function mifuncion() { /* This part is linked with an exercise in the HTML file */
     document.getElementById("demostracion").innerHTML = "Párrafo cambiado";
 }
+
+class Add {
+    constructor(...words) {
+        this.words = words;
+    }
+    //your code goes here
+    print() {
+          var array = []
+          for (let i = 0; i < this.words.length; i++) {
+              array.push("$" + this.words[i]);
+          }
+          console.log(array.join("") + "$" );
+      }
+  }
+  
+  var x = new Add("hehe", "hoho", "haha", "hihi", "huhu");
+  var y = new Add("this", "is", "awesome");
+  var z = new Add("lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit");
+  x.print();
+  y.print();
+  z.print();
